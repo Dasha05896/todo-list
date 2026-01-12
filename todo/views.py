6125b43b096edc931fe6ece2cb9d1fda0f524549
Daria Budovich <Budovich_Dasha@ukr.net>
2026-01-12 13:43:26 +0200

    add urls.py and views.py

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
 
-# Create your views here.
+from .models import Task, Tag
+from .forms import TaskForm, TagForm
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "todo/index.html"
+    context_object_name = "task_list"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "todo/task_form.html"
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "todo/task_form.html"
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "todo/task_confirm_delete.html"
+    success_url = reverse_lazy("todo:index")
+
+
+def toggle_task_status(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    task.is_done = not task.is_done
+    task.save()
+    return redirect("todo:index")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "todo/tag_list.html"
+    context_object_name = "tag_list"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    template_name = "todo/tag_form.html"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    form_class = TagForm
+    template_name = "todo/tag_form.html"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    template_name = "todo/tag_confirm_delete.html"
+    success_url = reverse_lazy("todo:tag-list")
